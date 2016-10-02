7678697410620bd1790db3a34b22d4383f20cba7
naichilab <naichilab@live.jp>
2016-10-02 17:52:27 +0900

    first commit

---
+++ Assets/DynamicCreateMesh.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshRenderer))]
+[RequireComponent (typeof(MeshFilter))]
+public class DynamicCreateMesh : MonoBehaviour
+{
+	// 変更箇所 : Materialを保持するようにする
+	[SerializeField]
+	private Material _mat;
+
+	private void Start ()
+	{
+		var mesh = new Mesh ();
+		mesh.vertices = new Vector3[] {
+			new Vector3 (0, 1f),
+			new Vector3 (1f, -1f),
+			new Vector3 (-1f, -1f),
+		};
+		mesh.triangles = new int[] {
+			0, 1, 2 
+		};
+
+		var filter = GetComponent<MeshFilter> ();
+		filter.sharedMesh = mesh;
+
+		// 変更箇所 : MeshRendererからMaterialにアクセスし、Materialをセットするようにする
+		var renderer = GetComponent<MeshRenderer> ();
+		renderer.material = _mat;
+	}
+}
---
+++ Assets/DynamicCreatePolygonMesh.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof(MeshRenderer))]
+[RequireComponent (typeof(MeshFilter))]
+public class DynamicCreatePolygonMesh : MonoBehaviour
+{
+	[SerializeField]
+	private Material _mat;
+
+	//頂点数
+	[SerializeField]
+	private int VerticesCount = 5;
+
+	//半径
+	[SerializeField]
+	private float Radius = 1f;
+
+	private void Start ()
+	{
+		if (VerticesCount < 3) {
+			Debug.LogError ("頂点数は３以上を指定してください。");
+			return;
+		}
+
+		List<Vector3> vertices = new List<Vector3> ();
+		List<int> triangles = new List<int> ();
+
+		//原点座標
+		vertices.Add (Vector3.zero);
+
+		//各頂点座標
+		for (int i = 1; i <= this.VerticesCount; i++) {
+			float rad = (90f - (360f / (float)this.VerticesCount) * (i - 1)) * Mathf.Deg2Rad;
+			float x = Mathf.Cos (rad);
+			float y = Mathf.Sin (rad);
+			vertices.Add (new Vector3 (x, y, 0));
+			triangles.Add (0);
+			triangles.Add (i);
+			triangles.Add (i == this.VerticesCount ? 1 : i + 1);
+		}
+
+		var mesh = new Mesh ();
+		mesh.vertices = vertices.ToArray ();
+		mesh.triangles = triangles.ToArray ();
+
+		var filter = GetComponent<MeshFilter> ();
+		filter.sharedMesh = mesh;
+
+		var renderer = GetComponent<MeshRenderer> ();
+		renderer.material = _mat;
+	}
+}
